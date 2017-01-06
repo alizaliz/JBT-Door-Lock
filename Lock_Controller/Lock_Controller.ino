e6d873bf27c42792f5a34dd451bca15e158a6ae0
Zain Ali <zain_ali8@hotmail.com>
2017-01-06 22:45:46 +1300

    Dropping servo for Stepper driver + motor

--- Lock_Controller/Lock_Controller.ino
+++ Lock_Controller/Lock_Controller.ino
@@ -1,64 +1,88 @@
 
+
+
+
 /*
 */
 
-#include <Servo.h>
+
+#include <DRV8834.h>
+#include <QueueArray.h>
 #include <SoftwareSerial.h>
 
 
 /* Components */
-const int servoPin = 9;
-SoftwareSerial XBee(2, 3); // RX, TX
+// Driver
+#define DIR 4
+#define STEP 5
+#define ENBL 6
+#define M0 7
+#define M1 8
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+DRV8834 stepper(MOTOR_STEPS, DIR, STEP, ENBL, M0, M1);
+//BTBee
+/*
+#define RX 2
+#define TX 3
+SoftwareSerial BTBee(RX, TX); */
 
 /*
  * Message constants:
  *  Expected message structure STX x ETX
  *  Where 'x' is the control character.
+ *  
  *  Control Characters:
  *    '+' Lock
  *    '-' Unlock
  *    '=' Query state
  *    '?' Settings
+ *        
+ *  '|' Delimmiter
 */
-const char preamble = '\x02'; // STX preamble
-const char postamble = '\x03'; // ETX postamble
+const char preamble = '2';//'\x02'; // STX preamble
+const char postamble = '3';//'\x03'; // ETX postamble
 const char lock = '+';
 const char unlock = '-';
 const char query = '=';
 const char setting = '?';
+const char delimmiter = '|';
 
-/* Servo - lock positions */
-int lockPos = 180;
-int unlockPos = 0;
-
-
-Servo servo; 
-int curPos;
+QueueArray <String> inMessageQ;
+QueueArray <String> outMessageQ;
+int curPos, speed;
 String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
+String tmp;
+
 
 
 void setup() {
-  servo.attach(servoPin); 
-  XBee.begin(38400);
+  //BTBee.begin(38400);
+  Serial.begin(9600);
+  //Memory allocation
   inputString.reserve(20);
-  servo.write(unlockPos);
-  curPos = unlockPos;
+  tmp.reserve(20);
+
+  // Default speed
+  speed = 120;
+  stepper.setRPM(speed);
+  // make sure we are in full speed mode
+  stepper.setMicrostep(1); 
+  stepper.disable(); // Keep it loosey goosey
 }
 
 void loop() {
 
-  
-  // print the string when a newline arrives:
-  if (stringComplete) {
-    switch(inputString.charAt(0)){
+  if (!inMessageQ.isEmpty()) {
+    tmp = inMessageQ.pop();
+    switch(tmp.charAt(0)){
       case lock:
-        servo.write(lockPos);
-        curPos = unlockPos;
+     
+        move();
         break;
       case unlock:
-        servo.write(unlockPos);
-        curPos = unlockPos;
+ 
+        move();
         break;
       case query:
         // Return curpos
@@ -69,19 +93,28 @@ void loop() {
     }
     
     /* Reset string paramters */
-    inputString = "";
-    stringComplete = false;
+    //inputString = "";
   }
 }
 
+void move() {
+
+ 
+
+}
+
+
+
+// Message recieved function
 int readState = 0;
 void serialEvent() {
   /* Parse serial input from BTBee */
+  //Serial.println("Recieved event");
   while (Serial.available()) { 
 
     char inChar = (char)Serial.read();
 
-    Serial.write(inChar);
+   Serial.write(inChar);
 
     switch (readState){
       case 0: // Search for preamble
@@ -91,7 +124,7 @@ void serialEvent() {
         break;
       case 1: // Collect characters
        if (inChar == postamble) { // Search for postamble - escape if found
-          stringComplete = true; // Set flag
+          inMessageQ.push(inputString); // Set flag
           readState = 0;
         } else if (inputString.length() < 20){ 
           inputString += inChar;
